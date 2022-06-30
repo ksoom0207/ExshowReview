7b123ac5ff6c060c260b317a728e37dee928ff69
숨 <ksoom0207@gmail.com>
2022-06-30 23:10:06 +0900

    MODIFY : url 상수화

--- js/post_write.js
+++ js/post_write.js
@@ -3,6 +3,8 @@ let title = document.getElementById("title");
 let content = document.getElementById("content");
 let post_write_button = document.getElementById("post_write_button");
 
+import { DEFAULT_POST_URL } from "../const_text.js";
+
 function post_check() {
     const post_write_data = {
         title: title.value,
@@ -18,29 +20,37 @@ function post_check() {
         return false;
     }
 
-    alert("게시글 작성 완료");
+    console.log(post_write_data);
 
-    fetch("http://localhost:3000/post/wirte", {
+    let url = DEFAULT_POST_URL + `write`
+    fetch(url, {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Accept": "application/json",
+            "x-access-token": document.cookie
         },
         body: JSON.stringify(post_write_data)
 
     }).then((response) => {
-        if (response.status === 400) {
 
+        if (response.status === 400) {
+            console.log(response.json);
         }
 
         if (response.status === 201) {
             alert("게시글 작성 완료");
             location.href = "post_main.html";
         };
+        console.log(response);
+        return response.json().then((data) => {
+            console.log(data);
+        });
 
     }).catch((error) => {
         console.log(error);
 
     })
+
 }
 
 post_write_button.addEventListener("click", post_check);
