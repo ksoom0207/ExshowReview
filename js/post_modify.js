40055d3b92ead429a677cf677ec11d605b80e571
숨 <ksoom0207@gmail.com>
2022-07-10 23:26:31 +0900

    MODIFY : 게시글 수정 fetch 코드 추가

--- js/post_modify.js
+++ js/post_modify.js
@@ -1,7 +1,7 @@
 
 let title = document.getElementById("title");
 let content = document.getElementById("content");
-let post_write_button = document.getElementById("post_write_button");
+let post_modify_button = document.getElementById("post_modify_button");
 
 import { DEFAULT_POST_URL } from "../const_text.js";
 
@@ -15,12 +15,14 @@ function post_data() {
     fetch(url, {
         method: "GET",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "x-access-token": document.cookie
         },
 
     }).then((response) => {
         if (response.status === 400) {
             console.log("fail");
+            //페이지 이동
         }
 
         if (response.status === 200) {
@@ -33,3 +35,53 @@ function post_data() {
     }).catch((error) => { console.log(error); });
 
 }
+
+post_data();
+
+function post_check() {
+    const post_write_data = {
+        title: title.value,
+        content: content.value
+    };
+    if (title.value.trim() == "" || title.value.replace(/^\s+|\s+$/gm, '') == "") {
+        alert("제목을 입력해주세요");
+        return false;
+    }
+
+    if (content.value.trim() == "" || content.value.replace(/^\s+|\s+$/gm, '') == "") {
+        alert("내용을 입력해주세요");
+        return false;
+    }
+
+    let url = DEFAULT_POST_URL + `write`
+    fetch(url, {
+        method: "PATCH",
+        headers: {
+            "Accept": "application/json",
+            "x-access-token": document.cookie
+        },
+        body: JSON.stringify(post_write_data)
+
+    }).then((response) => {
+
+        if (response.status === 400) {
+            console.log(response.json);
+        }
+
+        if (response.status === 201) {
+            alert("게시글 수정 완료");
+            location.href = "post_main.html";
+        };
+
+        return response.json().then((data) => {
+            console.log(data);
+        });
+
+    }).catch((error) => {
+        console.log(error);
+
+    })
+
+}
+
+post_modify_button.addEventListener("click", post_check);
