84d62f6fc46609c3ca35c30f69f9a7cd76b4e233
숨 <ksoom0207@gmail.com>
2022-08-03 23:19:11 +0900

    MODIFY : 변수명 수정

--- js/culture_detail.js
+++ js/culture_detail.js
@@ -42,23 +42,23 @@ async function fetch_method(url, options) {
 }
 
 
-// async function get_culture_info() {
+async function get_culture_info() {
 
-//     let url = DEFAULT_CULTURE_URL + culture_idx;
-//     let options = {
-//         method: "GET",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "x-access-token": document.cookie
-//         }
-//     }
+    let url = DEFAULT_CULTURE_URL + culture_idx;
+    let options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": document.cookie
+        }
+    }
 
-//     const data = await fetch_method(url, options);
-//     return data
+    const data = await fetch_method(url, options);
+    return data
 
-// }
+}
 
-// let result = await get_culture_info();
+let result = await get_culture_info();
 
 
 
@@ -68,26 +68,26 @@ let title_section = document.getElementById("title");
 
 let title_span = document.createElement("span");
 title_span.setAttribute('class', 'category');
-let text_title = document.createTextNode(culture_data.list[0].title);
+let text_title = document.createTextNode(result.title);
 title_span.append(text_title);
 title_section.append(title_span);
 
 //서브 정보(인덱스, 날짜, 아이디, 조회수)
 let sub_item = document.getElementsByClassName('sub');
 console.log(sub_item);
-let idx = document.createTextNode(culture_data.list[0].idx);
+let idx = document.createTextNode(result.idx);
 sub_item[0].append(idx);
 
-let start_date = document.createTextNode(culture_data.list[0].start_date.replace(/(\d{4})(\d{2})(\d)/, '$1-$2-$3'));
+let start_date = document.createTextNode(result.start_date.replace(/(\d{4})(\d{2})(\d)/, '$1-$2-$3'));
 sub_item[1].append(start_date);
 
-let end_date = document.createTextNode(culture_data.list[0].start_date.replace(/(\d{4})(\d{2})(\d)/, '$1-$2-$3'));
+let end_date = document.createTextNode(result.start_date.replace(/(\d{4})(\d{2})(\d)/, '$1-$2-$3'));
 sub_item[2].append(end_date);
 
-let place = document.createTextNode(culture_data.list[0].place);
+let place = document.createTextNode(result.place);
 sub_item[3].append(place);
 
-let view_count = document.createTextNode(culture_data.list[0].view_post_count);
+let view_count = document.createTextNode(result.view_post_count);
 sub_item[4].append(view_count);
 
 
@@ -96,7 +96,7 @@ let content = document.getElementById("content");
 let card = document.createElement("div");
 card.setAttribute("class", "culture-card");
 let img = document.createElement("img");
-img.setAttribute("src", culture_data.list[0].tumbnail);
+img.setAttribute("src", result.tumbnail);
 img.setAttribute("style", "width : 180px ; height : 240px");
 card.append(img);
 content.append(card);
